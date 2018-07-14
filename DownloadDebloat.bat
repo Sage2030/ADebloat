b4c3e833b3bdc471e9192248c2719b1fc144ccd3
Sage2030 <35377910+Sage2030@users.noreply.github.com>
2018-07-15 00:09:48 +0200

    Add files via upload

---
+++ DownloadDebloat.bat
@@ -0,0 +1,8 @@
+curl -o Scripts.zip https://codeload.github.com/W4RH4WK/Debloat-Windows-10/zip/master
+curl -o Windows10Debloater.ps1 https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10Debloater.ps1
+curl -o DisableCortana.zip https://www.howtogeek.com/wp-content/uploads/2016/07/Disable-Cortana.zip
+echo Non vanno se non esegui "set-executionpolicy unrestricted" >> Info.txt
+echo Creatori: >> Info.txt
+echo https://github.com/W4RH4WK/Debloat-Windows-10 >> Info.txt
+echo https://github.com/Sycnex/Windows10Debloater >> Info.txt
+echo https://www.howtogeek.com/265027/how-to-disable-cortana-in-windows-10/ >> Info.txt
